900e8120692c5e5269566e1eb236bcc7b5815ceb
scarletsakuya <31232410+scarletsakuya@users.noreply.github.com>
2018-09-08 17:04:56 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -4,5 +4,7 @@ if [ ! -z "${ROOT_PASSWORD}" ] && [ "${ROOT_PASSWORD}" != "root" ]; then
     echo "root:${ROOT_PASSWORD}" | chpasswd
 fi
 
+echo "Start Success !"
+
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 exec /usr/sbin/sshd -D -e
